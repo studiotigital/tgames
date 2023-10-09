863693d89f261b757a16ce6f0ddad59e885a24bd
Thiago3d Games <roll.brasil@gmail.com>
2023-10-09 19:57:15 -0300

    nova pasta

---
+++ assets/includes/config.php
@@ -0,0 +1,6 @@
+<?php
+$dbGM['host'] = 'localhost';
+$dbGM['name'] = 'gm';
+$dbGM['user'] = 'root';
+$dbGM['pass'] = '';
+$encryption = "vmbtrvw95105595885345**#3738s**A";
---
+++ templates/sports/content/game/new-games.php
@@ -0,0 +1,19 @@
+<div class="ad728list">
+{{ADS_TOP}}
+</div>
+{{GAMES_PLAYED}}
+
+{{TAG_NAME}}
+
+<div id="content">
+    {{ADS_300}}
+
+	{{NEW_GAMES_LIST}}
+</div>
+
+
+<div class="bottomtext">
+     GameFree Games. 
+     
+     <br> 
+</div>
