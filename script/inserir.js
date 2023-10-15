0aedf1c77875945ec9150700594792ac96f889c9
studiotigital <56325411+studiotigital@users.noreply.github.com>
2023-10-15 15:16:35 -0300

    Update inserir.js
    Signed-off-by: studiotigital <56325411+studiotigital@users.noreply.github.com>

--- script/inserir.js
+++ script/inserir.js
@@ -1,26 +1,6 @@
-
 $('#content').submit(function (event) {
     event.preventDefault();
     console.log('Evento de envio do formulário acionado.'); // Adicione essa linha
-    // Resto do seu código...
-});
-
-
-// Função para adicionar um novo jogo à página
-function addGame(name, url, imageUrl) {
-    var newGame = '<div class="post">';
-    newGame += '<a href="' + url + '">';
-    newGame += '<img src="' + imageUrl + '" alt="' + name + '">';
-    newGame += '<p class="post-name">' + name + '</p>';
-    newGame += '<span class="featured_icon"></span>';
-    newGame += '</a></div>';
-
-    $('#content').prepend(newGame);  // Adiciona o novo jogo no topo da lista
-}
-
-// Manipula o envio do formulário
-$('#content').submit(function (event) {
-    event.preventDefault();
     var gameName = $('#game-name').val();
     var gameUrl = $('#game-url').val();
     var gameImage = $('#game-image').val();
