ff14691ce0609bb3f75240815229c8ba88384d08
studiotigital <56325411+studiotigital@users.noreply.github.com>
2023-10-15 14:15:34 -0300

    Add files via upload
    Signed-off-by: studiotigital <56325411+studiotigital@users.noreply.github.com>

---
+++ script/inserir.js
@@ -0,0 +1,29 @@
+// Função para adicionar um novo jogo à página
+function addGame(name, url, imageUrl) {
+    var newGame = '<div class="post">';
+    newGame += '<a href="' + url + '">';
+    newGame += '<img src="' + imageUrl + '" alt="' + name + '">';
+    newGame += '<p class="post-name">' + name + '</p>';
+    newGame += '<span class="featured_icon"></span>';
+    newGame += '</a></div>';
+
+    $('#content').prepend(newGame);  // Adiciona o novo jogo no topo da lista
+}
+
+// Manipula o envio do formulário
+$('#game-form').submit(function (event) {
+    event.preventDefault();
+    var gameName = $('#game-name').val();
+    var gameUrl = $('#game-url').val();
+    var gameImage = $('#game-image').val();
+
+    // Adicione aqui qualquer validação necessária
+
+    // Chame a função addGame para adicionar o novo jogo à página
+    addGame(gameName, gameUrl, gameImage);
+
+    // Limpa os campos do formulário
+    $('#game-name').val('');
+    $('#game-url').val('');
+    $('#game-image').val('');
+});
