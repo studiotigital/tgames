f5b23bdc47324a9e4363e1f08f31af6a0b2a347d
studiotigital <56325411+studiotigital@users.noreply.github.com>
2023-10-15 17:35:17 -0300

    Update inserir.js
    Signed-off-by: studiotigital <56325411+studiotigital@users.noreply.github.com>

--- inserir.js
+++ inserir.js
@@ -1,29 +1,32 @@
-function adicionarJogo(event) {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+    var adicionarJogoBtn = document.getElementById("adicionarJogo");
+    adicionarJogoBtn.addEventListener("click", adicionarJogo);
 
-    // Captura os valores dos campos do formulário
-    var nomeDoJogo = document.getElementById("jogo-nome").value;
-    var urlDoJogo = document.getElementById("jogo-url").value;
+    function adicionarJogo() {
+        // Captura os valores dos campos do formulário
+        var nomeDoJogo = document.getElementById("jogo-nome").value;
+        var urlDoJogo = document.getElementById("jogo-url").value;
 
-    // Crie um novo elemento de jogo
-    var novoJogo = document.createElement("div");
-    novoJogo.className = "post";
+        // Crie um novo elemento de jogo
+        var novoJogo = document.createElement("div");
+        novoJogo.className = "post";
 
-    novoJogo.innerHTML = `
-        <a href="${urlDoJogo}">
-            <img src="${urlDoJogo}" alt="${nomeDoJogo}">
-            <p class="post-name">${nomeDoJogo}</p>
-            <span class='featured_icon'></span>
-        </a>
-    `;
+        novoJogo.innerHTML = `
+            <a href="${urlDoJogo}">
+                <img src="${urlDoJogo}" alt="${nomeDoJogo}">
+                <p class="post-name">${nomeDoJogo}</p>
+                <span class='featured_icon'></span>
+            </a>
+        `;
 
-    // Adicione o novo jogo à seção "content"
-    var content = document.getElementById("content");
-    content.appendChild(novoJogo);
+        // Adicione o novo jogo à seção "content"
+        var content = document.getElementById("content");
+        content.appendChild(novoJogo);
 
-    // Limpe os campos do formulário
-    document.getElementById("jogo-nome").value = "";
-    document.getElementById("jogo-url").value = "";
+        // Limpe os campos do formulário
+        document.getElementById("jogo-nome").value = "";
+        document.getElementById("jogo-url").value = "";
 
-    alert("Novo jogo adicionado com sucesso!");
-}
+        alert("Novo jogo adicionado com sucesso!");
+    }
+});
